1fff9add6accb5ffd8abb69d6c21ede25d2e3bc8
Kezimana Aime Angelo <kezangelo@gmail.com>
2023-07-02 12:16:06 +0200

    Add JWT_SECRET to settings.py

--- core/settings.py
+++ core/settings.py
@@ -35,6 +35,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('SECRET_KEY')
+JWT_SECRET = env('JWT_SECRET')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
