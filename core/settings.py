c79828088bc2ea74524d70b1d1e7c273d0c2462b
Kezimana Aime Angelo <kezangelo@gmail.com>
2023-07-04 14:35:41 +0200

    Feat: Add message app to settings.INSTALLED_APPS

--- core/settings.py
+++ core/settings.py
@@ -47,6 +47,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'account.apps.AccountConfig',
+    'account.apps.MessageConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
