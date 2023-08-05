719f5e2d8d720053d4a43858be7fdd9b58ca53ec
Kezimana Aime Angelo <kezangelo@gmail.com>
2023-08-05 23:43:45 +0200

    refactor: remove some unused endpoints to use djoser endpoints and configure djoser

--- account/urls.py
+++ account/urls.py
@@ -3,12 +3,5 @@ from django.urls import path
 from . import apis
 
 urlpatterns = [
-    path("register/", apis.RegisterApi.as_view(), name="register"),
-    path("me/", apis.UserApi.as_view(), name="me"),
-    path("logout/", apis.LogoutApi.as_view(), name="logout"),
-    path(
-        "change-password/",
-        apis.ChangePasswordApi.as_view(),
-        name="auth_change_password",
-    ),
+    path("jwt/delete/", apis.LogoutApi.as_view(), name="logout"),
 ]
